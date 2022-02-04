2d450c7e346488d26749ccae3df3f883959220f2
iKycaka <osujdenie@yandex.ru>
2022-02-04 23:21:59 +0300

    Laser mechanic done (without dmg)

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+
+    [SerializeField] float laserLength;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        ResetLaser();
+    }
+
+    void ResetLaser()
+    {
+        lineRenderer.SetPosition(1, new Vector3(0f, laserLength, 0f));
+    }
+
+    private void FixedUpdate()
+    {
+        int layerMask = 1 << 3;
+        layerMask = ~layerMask;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, laserLength, layerMask);
+        if (hit.collider != null)
+        {
+            lineRenderer.SetPosition(1, new Vector3(0f, Vector3.Distance(transform.position, hit.point), 0f));
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Hit");
+            }
+        }
+        else
+        {
+            ResetLaser();
+        }
+    }
+}
